2180e83ca708141ebcf37b3adb023f9f1ba100ad
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-03-25 22:24:57 +0100

    Add user rest api

--- src/main/java/com/example/backendclonereddit/resources/UserResource.java
+++ src/main/java/com/example/backendclonereddit/resources/UserResource.java
@@ -1,36 +1,67 @@
 package com.example.backendclonereddit.resources;
 
+import com.example.backendclonereddit.entities.User;
 import com.example.backendclonereddit.repositories.UserRepository;
-import com.example.backendclonereddit.services.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.backendclonereddit.utils.exceptions.UserNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(path = "/api/v1/users")
 public class UserResource {
 
-    private final UserService userService;
     private final UserRepository userRepository;
 
-//    -------------------  PATHS  -------------------
-    private static final String path = "/api/v1/users";
-    private final static String pathId = "/api/v1/users/{id}";
-    private final String pathEmail = "/api/v1/users/{email}";
-//    -------------------  PATHS  -------------------
-
-    public UserResource(UserService userService, UserRepository userRepository) {
-        this.userService = userService;
+    public UserResource(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @GetMapping(path = path)
-    public String getAllUsers() {
-        return "All users";
+    @GetMapping(path = "")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
-    @GetMapping(path = pathId)
-    public String getUserById(@PathVariable String id) {
-        return "User by id";
+    @GetMapping(path = "/{id}")
+    public User getUserById(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        return user.get();
+    }
+
+    @PostMapping(path ="")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser =  userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        user.setId(id);
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
     }
 
 }
