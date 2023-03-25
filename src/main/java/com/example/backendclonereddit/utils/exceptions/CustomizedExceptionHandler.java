a4ec227498a9551dcf8ef9ecad636accacad72d8
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-03-25 22:24:09 +0100

    Add exception handling and custom messages

--- src/main/java/com/example/backendclonereddit/utils/exceptions/CustomizedExceptionHandler.java
+++ src/main/java/com/example/backendclonereddit/utils/exceptions/CustomizedExceptionHandler.java
@@ -1,2 +1,41 @@
-package com.example.backendclonereddit.utils.exceptions;public class CustomizedExceptionHandler {
+package com.example.backendclonereddit.utils.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@ControllerAdvice
+public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                "Number of errors: "+ ex.getErrorCount() + " First one is: " + Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(),
+                request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
 }
--- src/main/java/com/example/backendclonereddit/utils/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/backendclonereddit/utils/exceptions/UserNotFoundException.java
@@ -1,2 +1,12 @@
-package com.example.backendclonereddit.utils.exceptions;public class UserNotFoundException {
+package com.example.backendclonereddit.utils.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super("User not found with: " + message);
+    }
+
 }
