aa6be6f4e9e2dc50c0a1300a30ff093c9f140a63
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-03-26 23:29:36 +0200

    Add endpoints for post resource

--- src/main/java/com/example/backendclonereddit/resources/PostResource.java
+++ src/main/java/com/example/backendclonereddit/resources/PostResource.java
@@ -1,4 +1,58 @@
 package com.example.backendclonereddit.resources;
 
+import com.example.backendclonereddit.configs.ApiPaths;
+import com.example.backendclonereddit.entities.Post;
+import com.example.backendclonereddit.repositories.PostRepository;
+import com.example.backendclonereddit.utils.exceptions.PostNotFoundException;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = ApiPaths.PostCtrl.CTRL)
 public class PostResource {
+    private final PostRepository postRepository;
+
+    public PostResource(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping(path = "")
+    public MappingJacksonValue getAllPosts() {
+        MappingJacksonValue mapping = new MappingJacksonValue(postRepository.findAll());
+        var simpleBeanPropertyFilter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "title", "content", "user", "comments");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("postFilter",simpleBeanPropertyFilter );
+
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping(path = "/{id}")
+    public MappingJacksonValue getPostById(@PathVariable Long id) throws PostNotFoundException {
+        Optional<Post> post = postRepository.findById(id);
+
+        if (post.isEmpty()) {
+            throw new PostNotFoundException("id-" + id);
+        }
+
+        MappingJacksonValue mapping = new MappingJacksonValue(post.get());
+        var simpleBeanPropertyFilter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "title", "content", "user", "comments");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("postFilter", simpleBeanPropertyFilter);
+
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
 }
--- src/main/java/com/example/backendclonereddit/resources/UserResource.java
+++ src/main/java/com/example/backendclonereddit/resources/UserResource.java
@@ -1,25 +1,36 @@
 package com.example.backendclonereddit.resources;
 
+import com.example.backendclonereddit.configs.ApiPaths;
+import com.example.backendclonereddit.entities.Comment;
+import com.example.backendclonereddit.entities.Post;
 import com.example.backendclonereddit.entities.User;
+import com.example.backendclonereddit.repositories.PostRepository;
 import com.example.backendclonereddit.repositories.UserRepository;
 import com.example.backendclonereddit.utils.exceptions.UserNotFoundException;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Filter;
 
 @RestController
-@RequestMapping(path = "/api/v1/users")
+@RequestMapping(path = ApiPaths.UserCtrl.CTRL)
 public class UserResource {
 
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
-    public UserResource(UserRepository userRepository) {
+    public UserResource(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @GetMapping(path = "")
@@ -28,7 +39,7 @@ public class UserResource {
     }
 
     @GetMapping(path = "/{id}")
-    public User getUserById(@PathVariable Long id) {
+    public User getUserById(@PathVariable Long id) throws UserNotFoundException {
         Optional<User> user = userRepository.findById(id);
         if (user.isEmpty()) {
             throw new UserNotFoundException("id-" + id);
@@ -54,14 +65,60 @@ public class UserResource {
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody User user) throws UserNotFoundException {
         Optional<User> userOptional = userRepository.findById(id);
         if (userOptional.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            throw new UserNotFoundException("id-" + id);
         }
         user.setId(id);
         userRepository.save(user);
         return ResponseEntity.noContent().build();
+    }
+
+//    ----------------- USER POSTS -----------------
+
+    @GetMapping(path = "/{id}/posts")
+    public MappingJacksonValue getAllPostsByUserId(@PathVariable Long id) throws UserNotFoundException {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(user.get().getPosts());
+        FilterProvider filters = new SimpleFilterProvider()
+                .addFilter("postFilter", SimpleBeanPropertyFilter.serializeAllExcept("user"));
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
+    }
+
+    @PostMapping(path = "/{id}/posts")
+    public ResponseEntity<Post> createPost(@PathVariable Long id, @Valid @RequestBody Post post) throws UserNotFoundException {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        User user = userOptional.get();
+        post.setUser(user);
+        Post savedPost =  postRepository.save(post);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedPost.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+//    ----------------- USER COMMENTS -----------------
+    @GetMapping(path = "/{id}/comments")
+    public List<Comment> getAllCommentsByUserId(@PathVariable Long id) throws UserNotFoundException {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        return user.get().getComments();
     }
 
 }
