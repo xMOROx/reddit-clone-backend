8706956b74cab526babc980fb081fda08cad02de
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-03-27 21:29:40 +0200

    Add repositories for post and comment

--- src/main/java/com/example/backendclonereddit/repositories/CommentRepository.java
+++ src/main/java/com/example/backendclonereddit/repositories/CommentRepository.java
@@ -1,12 +1,9 @@
 package com.example.backendclonereddit.repositories;
 
-import com.example.backendclonereddit.entities.Post;
+import com.example.backendclonereddit.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
