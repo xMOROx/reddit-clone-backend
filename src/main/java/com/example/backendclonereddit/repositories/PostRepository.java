81630f4e89f03611142b8305179568114fca92b8
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-03-27 22:30:28 +0200

    add post repository

--- src/main/java/com/example/backendclonereddit/repositories/PostRepository.java
+++ src/main/java/com/example/backendclonereddit/repositories/PostRepository.java
@@ -4,7 +4,6 @@ import com.example.backendclonereddit.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
