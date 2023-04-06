65a36f3329f1c815ff9691e875f67999021e93cb
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-04-06 22:45:14 +0200

    Add reply repository:

--- src/main/java/com/example/backendclonereddit/repositories/ReplyRepository.java
+++ src/main/java/com/example/backendclonereddit/repositories/ReplyRepository.java
@@ -1,2 +1,8 @@
-package com.example.backendclonereddit.repositories;public interface ReplyRepository {
+package com.example.backendclonereddit.repositories;
+
+import com.example.backendclonereddit.entities.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
 }
