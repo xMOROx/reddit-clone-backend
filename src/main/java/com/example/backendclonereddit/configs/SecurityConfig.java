baef2e07ca010f8ef4ad48f2419bdd8ef8ea61a5
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-04-06 16:22:28 +0200

    Add security configuration for app

--- src/main/java/com/example/backendclonereddit/configs/SecurityConfig.java
+++ src/main/java/com/example/backendclonereddit/configs/SecurityConfig.java
@@ -1,4 +1,25 @@
 package com.example.backendclonereddit.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig  {
+
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+//    {
+//
+//        return http.build();
+//    }
+
 }
