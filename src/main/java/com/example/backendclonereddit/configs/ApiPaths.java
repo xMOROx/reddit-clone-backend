dbe8c85d4b4d8a3426da83f9efa55c791d9df2b4
zajdel0202@gmail.com <zajdel0202@gmail.com>
2023-04-07 19:38:19 +0200

    Update SubReddit path

--- src/main/java/com/example/backendclonereddit/configs/ApiPaths.java
+++ src/main/java/com/example/backendclonereddit/configs/ApiPaths.java
@@ -3,6 +3,9 @@ package com.example.backendclonereddit.configs;
 public final class ApiPaths {
     private static final String BASE_PATH = "/api/v1";
 
+    public static final class SubRedditCtrl {
+        public static final String CTRL = BASE_PATH + "/subreddits";
+    }
     public static final class PostCtrl {
         public static final String CTRL = BASE_PATH + "/posts";
     }
